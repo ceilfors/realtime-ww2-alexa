76a67035bba09387daa0bdc805db67a0ac083efa
Wisen Tanasa <wisen@ceilfors.com>
2017-09-11 06:59:09 +0100

    Use alexa-sdk.

--- handler.js
+++ handler.js
@@ -1,21 +1,34 @@
 'use strict';
 
-// Returns a random integer between min (inclusive) and max (inclusive)
-const getRandomInt = (min, max) => Math.floor(Math.random() * ((max - min) + 1)) + min;
+const Alexa = require('alexa-sdk');
 
-module.exports.luckyNumber = (event, context, callback) => {
-  const upperLimit = event.request.intent.slots.UpperLimit.value || 100;
-  const number = getRandomInt(0, upperLimit);
-  const response = {
-    version: '1.0',
-    response: {
-      outputSpeech: {
-        type: 'PlainText',
-        text: `Your lucky number is ${number}`,
-      },
-      shouldEndSession: false,
-    },
-  };
+const HELP_MESSAGE = 'You can say tell me the latest news, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
 
-  callback(null, response);
+const handlers = {
+  'LaunchRequest': function () {
+      this.emit('GetLatestIntent');
+  },
+  'GetLatestIntent': function () {
+      const speechOutput = 'Nazi SS troops dressed as Poles are attacking German radio station in Gleiwitz, to provide false pretext for German attack on Poland.';
+      this.emit(':tell', speechOutput);
+  },
+  'AMAZON.HelpIntent': function () {
+      this.response.speak(HELP_MESSAGE).listen(HELP_REPROMPT);
+      this.emit(':responseReady');
+  },
+  'AMAZON.CancelIntent': function () {
+      this.emit(':tell', STOP_MESSAGE);
+  },
+  'AMAZON.StopIntent': function () {
+      this.emit(':tell', STOP_MESSAGE);
+  },
+};
+
+exports.handler = function (event, context, callback) {
+  const alexa = Alexa.handler(event, context, callback);
+  alexa.appId = 'amzn1.ask.skill.8955b37b-4975-4462-a4d9-4aba0ad647f0'
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
