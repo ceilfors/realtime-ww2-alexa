a600c85cda889b79f55d7d62992a90a54d9467a4
Wisen Tanasa <wisen@ceilfors.com>
2017-10-08 15:51:25 +0100

    Convert promise chain to async await.

--- packages/core/src/cache-tweets.js
+++ packages/core/src/cache-tweets.js
@@ -4,21 +4,25 @@ import slscrypt from '../node_modules/serverless-crypt/dists/slscrypt'
 
 const tweetRepository = new S3TweetRepository(process.env.TWEET_CACHE_BUCKET_NAME)
 
-const handler = (event, context, callback) => {
-  return createTwitterClient(process.env.TWITTER_REST_BASE_URL)
-    .then(client => {
-      const params = {
-        screen_name: 'RealTimeWWII',
-        trim_user: true,
-        exclude_replies: true,
-        include_rts: false,
-        count: 200,
-        tweet_mode: 'extended'
-      }
-      return client.get('statuses/user_timeline', params)
-    })
-    .then(tweets => tweetRepository.saveLatestTweets(tweets))
-    .catch(err => callback(err))
+const createParams = () => {
+  return {
+    screen_name: 'RealTimeWWII',
+    trim_user: true,
+    exclude_replies: true,
+    include_rts: false,
+    count: 200,
+    tweet_mode: 'extended'
+  }
+}
+
+const handler = async (event, context, callback) => {
+  try {
+    const client = await createTwitterClient(process.env.TWITTER_REST_BASE_URL)
+    const tweets = await client.get('statuses/user_timeline', createParams())
+    await tweetRepository.saveLatestTweets(tweets)
+  } catch (err) {
+    callback(err)
+  }
 }
 
 const createTwitterClient = async (twitterRestBaseUrl) => {
