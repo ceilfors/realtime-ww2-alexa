69f792ff0beed981fc25845bb640caab57de40ae
Wisen Tanasa <wisen@ceilfors.com>
2017-10-27 05:48:48 +0100

    Simplify spec by extracting chai and sinon duplication.

--- packages/core/test/lib/ssm-secrets-store.spec.js
+++ packages/core/test/lib/ssm-secrets-store.spec.js
@@ -1,20 +1,46 @@
 /* eslint-env mocha */
-import {expect} from 'chai'
+import chai, {expect} from 'chai'
 import sinon from 'sinon'
 import AWS from 'aws-sdk-mock'
 import SsmSecretsStore from '../../src/lib/ssm-secrets-store'
 
-describe.only('ssm secrets store', function () {
-  let ssm
+chai.use(function (_chai, utils) {
+  const Assertion = chai.Assertion
 
-  beforeEach(function () {
-    ssm = { getParameters: sinon.stub() }
-    ssm.getParameters.yields(null, {
+  Assertion.addMethod('calledWithNames', function (names) {
+    const stub = this._obj
+    const namesArgs = stub.getCall(0).args[0].Names
+    new Assertion(namesArgs).to.deep.equal(names)
+  })
+})
+
+const ssmGetParametersStub = () => {
+  const getParameters = sinon.stub()
+  getParameters.yieldParams = params => {
+    getParameters.yields(null, {
       data: {
-        Parameters: [],
+        Parameters: Object.keys(params).map(k => ({
+          Name: k,
+          Type: 'SecureString',
+          Value: params[k]
+        })),
         InvalidParameters: []
       }
     })
+  }
+  return getParameters
+}
+
+describe.only('SsmSecretsStore#getSecrets', function () {
+  let ssm, subject
+
+  beforeEach(function () {
+    subject = new SsmSecretsStore('/base/path/')
+  })
+
+  beforeEach(function () {
+    ssm = { getParameters: ssmGetParametersStub() }
+    ssm.getParameters.yieldParams({})
     AWS.mock('SSM', 'getParameters', ssm.getParameters)
   })
 
@@ -22,62 +48,26 @@ describe.only('ssm secrets store', function () {
     AWS.restore('SSM')
   })
 
-  context('when successfully retrieved SSM parameters', function () {
+  context('when successful', function () {
     it('should return an object for a single parameter', async function () {
       const parameterName = '/base/path/p1'
-      ssm.getParameters.yields(null, {
-        data: {
-          Parameters: [
-            { Name: parameterName,
-              Type: 'SecureString',
-              Value: 'v1' }
-          ],
-          InvalidParameters: []
-        }
-      })
-      const subject = new SsmSecretsStore('/base/path')
+      ssm.getParameters.yieldParams({[parameterName]: 'v1'})
       const secrets = await subject.getSecrets(['p1'])
       expect(secrets).to.not.be.undefined()
       expect(secrets).to.have.property(parameterName, 'v1')
     })
 
     it('should return an object for multiple parameters', async function () {
-      ssm.getParameters.yields(null, {
-        data: {
-          Parameters: [
-            { Name: '/base/path/p1',
-              Type: 'SecureString',
-              Value: 'v1' },
-            { Name: '/base/path/p2',
-              Type: 'SecureString',
-              Value: 'v2' },
-            { Name: '/base/path/p3',
-              Type: 'SecureString',
-              Value: 'v3' }
-          ],
-          InvalidParameters: []
-        }
+      ssm.getParameters.yieldParams({
+        '/base/path/p1': 'v1',
+        '/base/path/p3': 'v3'
       })
-      const subject = new SsmSecretsStore('/base/path')
       const secrets = await subject.getSecrets(['p1', 'p3'])
       expect(secrets).to.have.property('/base/path/p1', 'v1')
       expect(secrets).to.have.property('/base/path/p3', 'v3')
     })
 
     it('should call AWS.SSM with the correct parameters', async function () {
-      const subject = new SsmSecretsStore('/base/path')
-      await subject.getSecrets(['p1', 'p2', 'p3'])
-      expect(ssm.getParameters).to.be.called.calledWithExactly(
-        {
-          Names: ['/base/path/p1', '/base/path/p2', '/base/path/p3'],
-          WithDecryption: true
-        },
-        sinon.match.func
-      )
-    })
-
-    it('should not have double forward slash when constructing path', async function () {
-      const subject = new SsmSecretsStore('/base/path/')
       await subject.getSecrets(['p1'])
       expect(ssm.getParameters).to.be.called.calledWithExactly(
         {
@@ -88,16 +78,20 @@ describe.only('ssm secrets store', function () {
       )
     })
 
-    it('should not add slash if base path is not specified', async function () {
+    it('should call SSM with multiple names', async function () {
+      await subject.getSecrets(['p1', 'p2', 'p3'])
+      expect(ssm.getParameters).to.be.calledWithNames(['/base/path/p1', '/base/path/p2', '/base/path/p3'])
+    })
+
+    it('should call SSM without having double forward slash when constructing path', async function () {
+      await subject.getSecrets(['p1'])
+      expect(ssm.getParameters).to.be.calledWithNames(['/base/path/p1'])
+    })
+
+    it('should call SSM without adding slash if base path is not specified', async function () {
       const subject = new SsmSecretsStore('')
       await subject.getSecrets(['p1'])
-      expect(ssm.getParameters).to.be.called.calledWithExactly(
-        {
-          Names: ['p1'],
-          WithDecryption: true
-        },
-        sinon.match.func
-      )
+      expect(ssm.getParameters).to.be.calledWithNames(['p1'])
     })
   })
 
